70b25379287297bd1486e5c12c40faf97ec2f608
Oliver Henrique Maceira <oliver@ciandt.com>
2018-05-26 14:18:33 -0300

    Cria projeto simples ionic com tabs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Facebook } from '@ionic-native/facebook';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
+    Facebook,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController, private fb: Facebook) {}
 
-  constructor(public navCtrl: NavController) {
-
+  public fbLogin(): void {
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+      .then((res: FacebookLoginResponse) => console.log('Logged into Facebook!', res))
+      .catch((err: Error) => console.log('Error logging into Facebook', err));
   }
-
+  // this.fb.logEvent(this.fb.EVENTS.EVENT_NAME_ADDED_TO_CART);
 }
